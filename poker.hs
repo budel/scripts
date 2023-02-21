a56b8bb73c0622a6f3840b9809cc7c1c7ed8d355
Budel <budelchen@gmail.com>
2023-02-21 08:40:17 +0100

    scoring poker

--- poker.hs
+++ poker.hs
@@ -1,16 +1,62 @@
 import Data.List
 
-data Score = HighCard | OnePair | TwoPairs | ThreePairs | Straight | Flush | FullHouse | StraightFlush | RoyalFlush deriving (Eq, Ord, Show, Read, Bounded, Enum)  
+data Suit = C | D | H | S deriving (Eq, Ord, Show, Read, Bounded, Enum)
+type Rank = Int
+data Card = Card Rank Suit deriving (Eq, Ord, Show, Read, Bounded)
 
---order after sorting: [2..9] A J K Q T
-score :: [String] -> Score
-score (a:b:c:d:e)
-  | a!!0=='A' && b!!0=='J' && c!!0=='K' && d!!0=='Q' && e!!0=='T' = RoyalFlush
+toRank :: String -> Rank
+toRank text = 
+  case text of 
+    "T" -> 10
+    "J" -> 11
+    "Q" -> 12
+    "K" -> 13
+    "A" -> 14
+    d -> read d :: Int
+
+toCard :: String -> Card
+toCard text = Card (toRank (text!!0:"")) (read (text!!1:"") :: Suit)
+
+
+data Score = HighCard | OnePair | TwoPairs | Triple | Straight | Flush | FullHouse | SameFour | StraightFlush | RoyalFlush deriving (Eq, Ord, Show, Read, Bounded, Enum)  
+
+rank (Card r s) = r
+suit (Card r s) = s
+
+allTheSame :: (Eq a) => [a] -> Bool
+allTheSame xs = and $ map (== head xs) (tail xs)
+
+lenUniq = length . nub
+
+isStraight xs = all (==1) $ zipWith (-) (tail xs) xs
+isFlush xs = (length . nub $ xs) == 1
+isStraightFlush cards = isStraight (map rank cards) && isFlush (map suit cards)
+hasFourSame xs = allTheSame (init xs) || allTheSame (tail xs)
+hasTriple xs = allTheSame (take 3 xs) || allTheSame (take 3 (tail xs)) || allTheSame (take 3 (tail (tail xs)))
+hasPair (a:b:c:d:e:_) = a==b || b==c || c==d || d==e
+
+
+score :: [Card] -> Score
+score (x:xs)
+  | rank x==10 && isStraightFlush (x:xs) = RoyalFlush
+  | isStraightFlush (x:xs) = StraightFlush
+  | hasFourSame (map rank (x:xs)) = SameFour
+  | lenUniq (map rank (x:xs)) == 2 && hasPair (map rank (x:xs)) = FullHouse
+  | isFlush (map suit (x:xs)) = Flush
+  | isStraight (map rank (x:xs)) = Straight
+  | hasTriple (map rank (x:xs)) = Triple
+  | lenUniq (map rank (x:xs)) == 3 = TwoPairs
+  | hasPair (x:xs) = OnePair
+  | otherwise = HighCard
+  
 
 main = do
    content <- readFile "poker.txt"
    let linesOfFile = lines content
-   let hands = map (splitAt 15) linesOfFile
-   let p1scores = map sort . map words . map fst $ hands
-   print p1scores
+   let handsStr = map words $ linesOfFile
+   let hands = map (splitAt 5) . map (map toCard) $ handsStr
+   let p1scores = map score . map sort . map fst $ hands
+   let p2scores = map score . map sort . map snd $ hands
+   print (zip3 handsStr p1scores p2scores)
+
 
