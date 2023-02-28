8c6847851ed5e8bcb60daf893416903bcced1994
Budel <budelchen@gmail.com>
2023-02-28 20:46:58 +0100

    working on 816

--- 816/app/Main.hs
+++ 816/app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 import Data.Mod
-import Data.List (nub)
+import qualified Data.Set as DS
 
 type Mod5 = Mod 50515093
+
+s0Inv = 290707^%(-1)::Mod5
 
 sns :: Int -> Mod5
 sns = (map f [0..] !!)
@@ -13,15 +15,18 @@ p :: Int -> (Mod5, Mod5)
 p n = (sns (2*n), sns (2*n+1))
 
 dist :: Num a => (a, a) -> (a, a) -> a
-dist (p1,p2) (q1,q2) = (q1-p1)*(q1-p1) + (q2-p2)*(q2-p2)
+dist (p1,p2) (q1,q2) = abs (p1-q1) + abs (p2-q2)
+euclDist :: Num a => (a, a) -> (a, a) -> a
+euclDist (p1,p2) (q1,q2) = (q1-p1)*(q1-p1) + (q2-p2)*(q2-p2)
 
-distOf2 :: Num a => [(a, a)] -> a
-distOf2 xs = dist (head xs) (xs!!1)
-
-combinationsWRep :: Eq a => [a] -> Int -> [[a]]
-combinationsWRep xs n = filter ((n==).length.nub) $ mapM (const xs) [1..n]
+distOf2 :: Num a => DS.Set (a, a) -> a
+distOf2 xs = dist (DS.elemAt 0 xs) (DS.elemAt 1 xs)
 
 main :: IO ()
 main = do
-    let ps = map p [0..14]
-    print $ minimum (map distOf2 $ combinationsWRep ps 2)
+    --let ps = map p [0..2000000]
+    let ps = map p [0..13]
+    let psSet = DS.fromList ps
+    let combs = DS.filter (\x->length x==2) . DS.powerSet $ psSet
+    print $ minimum . map distOf2 $ DS.toList combs
+    print s0Inv
